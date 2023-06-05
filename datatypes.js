7577155ae60f7fcaab752c5e4e0cb79479dfeeb8
andrewbantly <andrewbantly@gmail.com>
2023-06-05 16:58:36 -0700

    git init

---
+++ datatypes.js
@@ -0,0 +1,11 @@
+"use strict";
+let fname = 'Murphy';
+let lowername = fname.toLowerCase();
+console.log(lowername);
+let c = 0 /* Color.Red */;
+let swapNubs;
+function swapNumbers(num1, num2) {
+    return [num2, num1];
+}
+swapNubs = swapNumbers(10, 20);
+swapNubs[0];
---
+++ datatypes.ts
@@ -0,0 +1,22 @@
+let fname : string = 'Murphy';
+
+let lowername = fname.toLowerCase();
+
+console.log(lowername)
+
+const enum Color {
+    Red,
+    Green,
+    Blue
+}
+let c: Color = Color.Red;
+
+let swapNubs: [number, number];
+
+function swapNumbers(num1:number,num2:number): [number,number] {
+    return[num2, num1]
+}
+
+swapNubs = swapNumbers(10, 20);
+
+swapNubs[0];
